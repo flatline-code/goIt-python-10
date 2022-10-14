e061d48c3e0aad8fbf146d1c69c22e9a30df91e4
Oleksii Kukhariev <keysneuro@gmail.com>
2022-10-14 09:34:33 +0300

    first update

--- bot_helper_with_classes.py
+++ bot_helper_with_classes.py
@@ -8,36 +8,35 @@ def input_error(handler):
         except Exception as e:
             error_string = e.args[0]
             if "show_phone()" in error_string: 
-                print('enter name')
+                return 'enter name' 
             else:
-                print('enter name and phone')
+                return 'enter name and phone' 
     return wrapper
 
 def stop():
-    print('Good bye!')
+    return 'Good bye!'
 
 def greeting():
-    print('How can I help you?')
+    return 'How can I help you?' 
 
 @input_error
 def add(name, phone):
     if address_book.data.get(name):
-        print('Contact already exist')
-        return
+        return 'Contact already exist' 
     
     record = Record(name)
     record.add_phone(phone)
     address_book.add_record(record)
-    print('new contact added')
+    return 'new contact added'
 
 @input_error
 def add_phone(name, phone):
     if address_book.data.get(name):
         record = address_book.data[name]
         record.add_phone(phone)
-        print(f'A new phone {phone} has been added to contact {name}')
+        return f'A new phone {phone} has been added to contact {name}' 
     else:
-        print('Сontact does not exist')
+        return 'Сontact does not exist' 
 
 @input_error
 def change(name, phone):
@@ -45,18 +44,18 @@ def change(name, phone):
     record = address_book.data[name]
 
     if record.change_phone(phone, new_phone) is True:
-        print(f'{name} phone number changed to {new_phone}')
+        return f'{name} phone number changed to {new_phone}' 
     else:
-        print('Phone number does not exist')
+        return 'Phone number does not exist' 
 
 @input_error
 def delete_phone(name, phone):
     record = address_book.data[name]
 
     if record.delete_phone(phone) is True:
-        print(f'Contact: {name} phone {phone} deleted')
+        return f'Contact: {name} phone {phone} deleted' 
     else:
-        print('Phone number does not exist')
+        return 'Phone number does not exist' 
 
 @input_error
 def show_phone(name):
@@ -64,20 +63,21 @@ def show_phone(name):
         phones_list = []
         for phone in address_book.data[name].phones:
             phones_list.append(phone.value)
-        print(phones_list)
+        return phones_list 
     else:
-        print('no such name')
+        return 'no such name' 
 
 def show_all():
     if not address_book.data:
-        print('nothing to show')
-        return
+        return 'nothing to show'
 
+    all_contacts = ''
     for name, record in address_book.items():
         phones_list = []
         for phone in record.phones:
             phones_list.append(phone.value)
-        print(f'{name} | phones: {phones_list}')
+        all_contacts += f'{name} | phones: {phones_list}\n'
+    return all_contacts
 
 def main():  
     commands_without_input = {
@@ -103,14 +103,16 @@ def main():
         user_inputs = user_command_with_inputs[1:]
 
         if user_command in ['exit', 'close', 'good bye']:
-            stop()
+            print(stop())
             break
         
         if commands_without_input.get(user_command):
-            commands_without_input[user_command]()
+            result = commands_without_input[user_command]()
+            print(result)
             continue
         elif commands_with_input.get(command):
-            commands_with_input[command](*user_inputs)
+            result = commands_with_input[command](*user_inputs)
+            print(result)
             continue
         else:
             print('unknown command')
